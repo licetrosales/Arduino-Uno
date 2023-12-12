e99775a798eb02f8238f54c8696351a140a0695a
licetrosales <54541600+licetrosales@users.noreply.github.com>
2023-12-12 16:39:49 +0100

    Update sketch.ino
    Add Led to pin 8

--- sketch.ino
+++ sketch.ino
@@ -5,13 +5,14 @@ Erstellt: 13.12.2023
 // Bibliotheken
 #include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
-
+#define schalter 8
+#define led 7
 // Erstell rtc and lcd Object:
 RTC_DS1307 rtc;
 LiquidCrystal_I2C lcd(0x27, 20, 4); // LCD-Adresse 0x27, 20 Zeichen, 4 Zeilen
-
-
 char daysOfTheWeek[7][12] = {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
+
+bool zustand = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -25,12 +26,15 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
+
+  pinMode(schalter, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // Date Time
   DateTime now =rtc.now();
-
+  digitalWrite(led, HIGH);
   Serial.print("Current time: ");
   Serial.print(now.year(), DEC);
   Serial.print('/');
@@ -67,10 +71,14 @@ void loop() {
   lcd.print('.');
   lcd.print(now.day(), DEC);
 
+  
+  
+
   lcd.setCursor(0,3);
   lcd.print("Wochentag: ");
   lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
-
+  digitalWrite(led, LOW);
   delay(1000);
+  
    
 }
